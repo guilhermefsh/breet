9bd08208aaa32a36a29972b4260bf3a320565b49
Guilherme Ferreira <gferreiradasilva96@gmail.com>
2024-10-28 10:56:15 -0300

    Initial commit

---
+++ src/main/java/fshproject/breet/BreetApplication.java
@@ -0,0 +1,13 @@
+package fshproject.breet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fshproject/breet/BreetApplicationTests.java
@@ -0,0 +1,13 @@
+package fshproject.breet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
